d3760fc38134596053609bcbfbd9ae968172c274
skylize <eveningsky@gmail.com>
2025-01-06 16:22:03 -0500

    Initial commit, deps-new scaffold
    Created new project using seancorfield/deps-new and created git repo

---
+++ src/skylize/clobber.clj
@@ -0,0 +1,6 @@
+(ns skylize.clobber)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (prn x "Hello, World!"))
---
+++ test/skylize/clobber_test.clj
@@ -0,0 +1,7 @@
+(ns skylize.clobber-test
+  (:require [clojure.test :refer :all]
+            [skylize.clobber :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
